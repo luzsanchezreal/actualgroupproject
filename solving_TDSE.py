629ac3f419fb68549178715ae4c44d7a3e32125f
luzsanchezreal <117984321+luzsanchezreal@users.noreply.github.com>
2022-11-22 10:27:32 +0000

    Added documentation, and saving as mp4

--- solving_TDSE.py
+++ solving_TDSE.py
@@ -16,12 +16,12 @@ import matplotlib.animation as animation
 
 
 # Input parameters
-Nx = 250
+# Define space & time variables
+Nx = 500
+Nt = Nx
 
-#defining space & time variables
 xmin = -5
 xmax = 5
-Nt = 100
 tmin = 0
 tmax = 60
 
@@ -41,7 +41,6 @@ Numpy packages can be used to form a string as the potential
 Defining a function to be returned as the default potential 
 '''
 
-
 def f(x_array):
     return x_array**2
 
@@ -53,7 +52,7 @@ def V():
 
     exec(command, globals())
 
-    try:
+    try: # add exceptions for errors from program execution
         return f(x_array)
     except:
         print("\nV(x) input does not compute.")
@@ -66,20 +65,23 @@ def V():
 
 VV = V()
 
-
 '''
 Converting V into a Diagonal matrix and calculating small psi
 '''
 Vmatrix = diags(VV)
 psi = np.exp(-(x_array+2)**2)
 
+# Calculating finite difference elements
 dt = t_array[1] - t_array[0]
 dx = x_array[1] - x_array[0]
 
+# Calculating the Hamiltonian matrix
 H = -0.5 * FinDiff(0, dx, 2).matrix(x_array.shape) + Vmatrix
+# FinDiff above is a way of representing our partial derivatives. FinDiff objects behave like operators; they have a tuple of the form (axis, spacing, degree) in the argument list for each partial derivative. Note the last argument stands for the degree of the derivative (in our case 2).
+
 
 '''
-Apply boundary conditions to the Hamiltonian
+Appling boundary conditions to the Hamiltonian
 '''
 
 H[0, :] = H[-1, :] = 0
@@ -88,13 +90,12 @@ H[0, 0] = H[-1, -1] = 1
 '''
 Calculating U
 '''
-
+# eye returns all elements to 0 except diagonal to 1.
 I_plus = eye(Nx) + 1j * dt / 2. * H
 I_minus = eye(Nx) - 1j * dt / 2. * H
-U = inv(I_minus).dot(I_plus)
+U = inv(I_minus).dot(I_plus) # we take the dot product, giving U as a discretized version of the time propagation operator in the Crank-Nicholson scheme
 
-
-# making a list of all the calculations
+# Iterating over each time, appending each calculation of psi to make a list of all the calculations
 psi_list = []
 for t in t_array:
     psi = U.dot(psi)
@@ -103,72 +104,23 @@ for t in t_array:
 
 
 """
-Creates a Data file 
+Creating a Data file
 """
-
-def download_data(Nx=500, xmin=-5, xmax=5, tmin=0, tmax=20, V_x='x**2'):
-
-    # Calculate grid, and initial wave function
-    Nt = Nx
-    x = np.linspace(xmin, xmax, Nx)
-    t_array = np.linspace(tmin, tmax, Nt)
-    psi = np.exp(-(x+2)**2)
-
-    # Calculate finite difference elements
-    dt = t_array[1] - t_array[0]
-    dx = x[1] - x[0]
-
-    # Put V(x) array values into the diagonal elements of an empty matrix
-    V_x_matrix = diags(VV)
-
-    # Calculate the Hamiltonian matrix
-    H = -0.5 * FinDiff(0, dx, 2).matrix(x.shape) + V_x_matrix
-
-    # Apply boundary conditions to the Hamiltonian
-    H[0, :] = H[-1, :] = 0
-    H[0, 0] = H[-1, -1] = 1
-
-    # Calculate U
-    I_plus = eye(Nx) + 1j * dt / 2. * H
-    I_minus = eye(Nx) - 1j * dt / 2. * H
-    U = inv(I_minus).dot(I_plus)
-
-    # Iterate over each time, appending each calculation of psi to a list
-    psi_list = []
-    for t in t_array:
-        psi = U.dot(psi)
-        psi[0] = psi[-1] = 0
-        psi_list.append(np.abs(psi))
-
-    while True:
-        data_input = input("\nWould you like to download a csv file of the data? (y/n)")
-        if data_input == 'y':
-
-            df = pd.DataFrame({'x': x, 'psi': psi_list})
-            df.to_csv('psi_data.csv')
-            print("\nData has been downloaded.")
-            break
-        elif data_input ==  'n':
-            print("\nNo download chosen. ")
-            break   
-        else:
-            print("\nInvalid response.")
-            print("Please enter... y (yes for download) or n (no download).")
-
-
-download_data(Nx, xmin, xmax, tmin, tmax, V_x=VV)
+df = pd.DataFrame({'x': x_array, 'psi': psi_list})
+df.to_csv('psi_data.csv')
 
 
 """
-Plots and Animates Data
+We now have a numerical solution stored in our psi_data. 
+In order to visualize this, we will make plots and animate the data.
 """
-
+# in 2D
 fig, ax = plt.subplots()
 ax.set_xlabel("x [arb units]")
 ax.set_ylabel("$|\Psi(x, t)|$", color="C0")
 ax.set_xlim(x_array[0], x_array[-1])
 ax.set_ylim(0, 1.5)
-ax.grid()
+ax.grid() # This shows time increasing vertically on the y axis, with a probability density moving back and forth
 
 ax_twin = ax.twinx()
 ax_twin.plot(x_array, VV, color="C1")
@@ -180,6 +132,7 @@ def run(psi):
     line.set_data(x_array, np.abs(psi)**2)
     return line,
 
-ani = animation.FuncAnimation(fig, run, psi_list, interval=10)
+ani = animation.FuncAnimation(fig, run, psi_list, interval=10) # animating to show the time dependence
+ani.save("particle_in_a_well.mp4", fps=120, dpi=300) # saving as an mp4
 
 plt.show()
